3e79c29e68c800e5796d61f30e7a846f582e1cf7
John Watson <john.watson1@defra.gov.uk>
2025-07-28 15:44:48 +0100

    Split browserstack yaml files

--- playwright.local.browserstack.config.js
+++ playwright.local.browserstack.config.js
@@ -1,15 +1,12 @@
 import { defineConfig } from '@playwright/test'
 import baseConfig from './playwright.browserstack.config.js'
 
-// Local testing with BrowserStack SDK will use browserstack.yml configuration
-// and enable local tunnel automatically
 export default defineConfig({
   ...baseConfig,
   retries: 0,
 
   use: {
     ...baseConfig.use,
-    // Override baseURL for local testing
     baseURL: 'http://localhost:3000'
   }
 })
