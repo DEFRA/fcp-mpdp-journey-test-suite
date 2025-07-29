062258e17fe0d5ed027d1d6b9dd99ca6459f459b
John Watson <42574977+johnwatson484@users.noreply.github.com>
2025-07-29 12:42:16 +0100

    Update proxy (#11)

--- playwright.browserstack.config.js
+++ playwright.browserstack.config.js
@@ -3,14 +3,12 @@ import { ProxyAgent, setGlobalDispatcher } from 'undici'
 import { bootstrap } from 'global-agent'
 import baseConfig from './playwright.config.js'
 
-if (process.env.HTTP_PROXY) {
-  const dispatcher = new ProxyAgent({
-    uri: 'http://localhost:3128'
-  })
-  setGlobalDispatcher(dispatcher)
-  bootstrap()
-  global.GLOBAL_AGENT.HTTP_PROXY = 'http://localhost:3128'
-}
+const dispatcher = new ProxyAgent({
+  uri: 'http://localhost:3128'
+})
+setGlobalDispatcher(dispatcher)
+bootstrap()
+global.GLOBAL_AGENT.HTTP_PROXY = 'http://localhost:3128'
 
 // BrowserStack SDK will use browserstack.yml for configuration
 export default defineConfig({
