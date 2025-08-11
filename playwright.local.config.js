37cb98b2dab65278afdfc3280ccb1eb06ff774ef
John Watson <42574977+johnwatson484@users.noreply.github.com>
2025-08-11 09:42:30 +0100

    Add 404 error page tests (#21)

--- playwright.local.config.js
+++ playwright.local.config.js
@@ -3,19 +3,14 @@ import baseConfig from './playwright.config.js'
 
 export default defineConfig({
   ...baseConfig,
+  workers: 1,
+  retries: 0,
+
   use: {
     ...baseConfig.use,
     baseURL: 'http://localhost:3000',
-    headless: false
-  },
-
-  ...(process.env.DEBUG && {
-    workers: 1,
-    use: {
-      ...baseConfig.use,
-      headless: false,
-      trace: 'on',
-      video: 'on'
-    }
-  })
+    headless: false,
+    trace: 'on',
+    video: 'on'
+  }
 })
---
+++ test/specs/404.test.js
@@ -0,0 +1,23 @@
+import { test, expect } from '@playwright/test'
+import AxeBuilder from '@axe-core/playwright'
+
+test.describe('404 page', () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto('/does-not-exist')
+  })
+
+  test('Should display the correct title', async ({ page }) => {
+    await expect(page).toHaveTitle('Page not found - Find farm and land payment data - GOV.UK')
+  })
+
+  test('Should display the not found content', async ({ page }) => {
+    await expect(page.locator('h1')).toHaveText('Page not found')
+    await expect(page.locator('p').nth(0)).toHaveText('If you typed the web address, check it is correct.')
+    await expect(page.locator('p').nth(1)).toHaveText('If you pasted the web address, check you copied the entire address.')
+  })
+
+  test('Should meet WCAG 2.2 AA', async ({ page }) => {
+    const results = await new AxeBuilder({ page }).analyze()
+    expect(results.violations).toHaveLength(0)
+  })
+})
