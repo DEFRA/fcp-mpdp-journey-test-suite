6c78360da10e507f7799cc388e19189bd1d5ba1f
Rana Salem <ranatasalem@gmail.com>
2025-08-26 09:24:09 +0100

    wip

--- test/specs/accessibility.test.js
+++ test/specs/accessibility.test.js
@@ -1,4 +1,6 @@
 import { test, expect } from '@playwright/test'
+import { expectNewPageLink } from '../../utils/new-page-link-expect.js'
+import { expectPhaseBanner } from '../../utils/phase-banner-expect.js'
 
 test.describe('Accessibility page', () => {
   test.beforeEach(async ({ page }) => {
@@ -7,5 +9,26 @@ test.describe('Accessibility page', () => {
 
   test('Should display the correct title', async ({ page }) => {
     await expect(page).toHaveTitle('Accessibility statement for Find Farm and Land Payment Data - Find farm and land payment data - GOV.UK')
+  })
+
+  test('Should display the correct heading', async ({ page }) => {
+    await expect(page.locator('h1').toHaveTest('Accessibility statement for Find Farm and Land Payment Data'))
+  })
+
+  test('Should have a back link that directs to the previous page', async ({ page }) => {
+    const backLink = page.locator('#back-link')
+
+    await expect(backLink).toHaveText('Back')
+    await expect(backLink).toHaveAttribute('href', '{{ referer }}')
+  })
+
+  test('Should display the correct phase banner', async ({ page, context }) => {
+    await expectPhaseBanner({ page })
+
+    await expectNewPageLink(
+      context,
+      page.locator('.govuk-phase-banner .govuk-link'),
+      'https://defragroup.eu.qualtrics.com/jfe/form/SV_1FcBVO6IMkfHmbs'
+    )
   })
 })
