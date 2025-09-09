037b10457a0a29f733e1459db318593aef79de51
Rana Salem <ranatasalem@gmail.com>
2025-09-09 16:06:53 +0100

    add journey tests for details page

---
+++ test/specs/details.test.js
@@ -0,0 +1,122 @@
+import { test, expect } from '@playwright/test'
+import { accessibilityTest } from '../accessibility.test.js'
+import { securityTest } from '../security.test.js'
+import { expectPhaseBanner } from '../../utils/phase-banner-expect.js'
+import { expectNewTab } from '../../utils/new-tab-expect.js'
+import { expectRelatedContent } from '../../utils/related-content-expect.js'
+
+test.describe('Details page', () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto('/details?payeeName=Feeney%20and%20Sons&partPostcode=GO15&searchString=Sons&page=1')
+  })
+
+  test('Should display the correct title', async ({ page }) => {
+    await expect(page).toHaveTitle('Feeney and Sons - Find farm and land payment data - GOV.UK')
+  })
+
+  test('Should display the correct heading', async ({ page }) => {
+    const heading = await page.locator('h1')
+    expect(heading).toHaveText('Feeney and Sons')
+  })
+
+  test('Should display the correct phase banner', async ({ page, context }) => {
+    await expectPhaseBanner({ page })
+
+    await expectNewTab(
+      context,
+      page.locator('.govuk-phase-banner .govuk-link'),
+      'https://defragroup.eu.qualtrics.com/jfe/form/SV_1FcBVO6IMkfHmbs'
+    )
+  })
+
+  test('Should have a back link that directs to the results page', async ({ page }) => {
+    const backLink = page.locator('#back-link')
+    await expect(backLink).toHaveText('Back to results')
+
+    const href = await backLink.getAttribute('href')
+    expect(href).toBe('/results?searchString=Sons&page=1')
+
+    await backLink.click()
+    await expect(page).toHaveURL(/\/results\?searchString=Sons&page=1$/)
+  })
+
+  test('Summary panel should display correct totals', async ({ page }) => {
+    const totalSchemes = await page.locator('#total-schemes').innerText()
+    const totalAmount = await page.locator('#mpdp-summary-panel p.govuk-heading-m').innerText()
+    const dateRange = await page.locator('#date-range').innerText()
+
+    expect(totalSchemes).toBe('Payments from 1 schemes')
+    expect(totalAmount).toBe('£33,577.90')
+    expect(dateRange).toBe('1 April 2023 to 31 March 2024')
+  })
+
+  test.describe('Related Content', () => {
+    test('Related Content section contains correct information', async ({ page }) => {
+      const links = [
+        { selector: '#fflm-link', text: 'Funding for farmers, growers and land managers' }
+      ]
+      await expectRelatedContent({ page, links })
+    })
+
+    test('Funding for farmers, growers and land managers directs to the correct page', async ({ page, context }) => {
+      await expectNewTab(
+        context,
+        page.locator('#fflm-link'),
+        'https://www.gov.uk/guidance/funding-for-farmers'
+      )
+    })
+  })
+
+  test.describe('Report a problem', () => {
+    test.beforeEach(async ({ page }) => {
+      const reportProblemToggle = page.locator('#report-problem')
+      await reportProblemToggle.click()
+    })
+
+    test('Report a problem section should expand and contain correct information', async ({ page }) => {
+      const rpaEmailLink = page.locator('#rpa-email')
+      const sfiQueryFormLink = page.locator('#sfi-query-form')
+      const callChargesLink = page.locator('#call-charges')
+
+      await expect(rpaEmailLink).toHaveAttribute('href', 'mailto:ruralpayments@defra.gov.uk')
+
+      await expect(sfiQueryFormLink).toHaveText('SFI pilot query form')
+      await expect(sfiQueryFormLink).toHaveAttribute(
+        'href',
+        'https://www.gov.uk/government/publications/sustainable-farming-incentive-pilot-query-form'
+      )
+
+      await expect(callChargesLink).toHaveText('Find out about call charges')
+      await expect(callChargesLink).toHaveAttribute('href', 'https://www.gov.uk/call-charges')
+    })
+
+    test('SFI query form link should direct to the correct page', async ({ page, context }) => {
+      await expectNewTab(
+        context,
+        page.locator('#sfi-query-form'),
+        'https://www.gov.uk/government/publications/sustainable-farming-incentive-pilot-query-form'
+      )
+    })
+
+    test('Call charges link should direct to the correct page', async ({ page, context }) => {
+      await expectNewTab(
+        context,
+        page.locator('#call-charges'),
+        'https://www.gov.uk/call-charges'
+      )
+    })
+  })
+
+  test('More actions links should exist and have correct targets', async ({ page }) => {
+    await expect(page.locator('#new-search-link')).toHaveAttribute('href', '/search')
+    await expect(page.locator('#print-link')).toHaveAttribute('href', 'javascript:window.print()')
+  })
+
+  test('Should meet WCAG 2.2 AA', async ({ page }) => {
+    await accessibilityTest(page)
+  })
+
+  test('Should meet security standards', async ({ page }) => {
+    await securityTest(page.url())
+  })
+})
