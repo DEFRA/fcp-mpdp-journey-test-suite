e23a696a74487f24258680e7492aa1b425756522
Rana Salem <ranatasalem@gmail.com>
2025-09-17 08:35:12 +0100

    update journey tests on downloading search results & downloading all scheme payment data

--- test/specs/results.test.js
+++ test/specs/results.test.js
@@ -158,7 +158,7 @@ test.describe('Results page', () => {
     test('Download all scheme payment data link should download a .CSV file', async ({ page }) => {
       const downloadLink = page.locator('#download-all-scheme-payment-data-link')
 
-      await expect(downloadLink).toHaveText('download all scheme payment data')
+      await expect(downloadLink).toHaveText('download all scheme payment data (4.7MB)')
       await expect(downloadLink).toHaveAttribute('href', '/all-scheme-payment-data/file')
 
       const [download] = await Promise.all([
