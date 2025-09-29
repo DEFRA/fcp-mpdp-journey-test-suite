f19f8ed694701a6f1c850993e57ccef4314491e5
Rana Salem <127218837+rtasalem@users.noreply.github.com>
2025-09-30 00:00:20 +0100

    MPDP-710 Cont. - Fix typo (#45)

--- test/expect/more-actions.js
+++ test/expect/more-actions.js
@@ -2,7 +2,7 @@ import { expect } from '@playwright/test'
 import { isAndroid } from '../../utils/devices.js'
 
 export async function expectMoreActionsSection (page, testInfo) {
-  if (isAndroid(testInfo)) {
+  if (!isAndroid(testInfo)) {
     await expect(page.locator('#new-search-link')).toHaveAttribute('href', '/search')
     await expect(page.locator('#print-link')).toHaveAttribute('href', '#')
   }
