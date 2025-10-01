07cebd851a61d7c13e77835d00891b8543c761a3
Rana Salem <ranatasalem@gmail.com>
2025-10-01 10:19:24 +0100

    pr review: update const name for page title

--- test/expect/title.js
+++ test/expect/title.js
@@ -2,6 +2,7 @@ import { expect } from '@playwright/test'
 
 export async function expectTitle (page, expectedTitle) {
   const title = await page.title()
-  const _title = expectedTitle ? `${expectedTitle} - ` : ''
-  expect(title).toBe(`${_title}Find farm and land payment data - GOV.UK`)
+  const pageTitle = expectedTitle ? `${expectedTitle} - ` : ''
+
+  expect(title).toBe(`${pageTitle}Find farm and land payment data - GOV.UK`)
 }
