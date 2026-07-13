02b3c0ed52c44c507093e17073b1713b979fd422
John Watson <john.watson1@defra.gov.uk>
2026-07-13 14:05:52 +0100

    Use href checking for links

--- test/expect/page-url.js
+++ test/expect/page-url.js
@@ -3,8 +3,5 @@ import { expect } from '@playwright/test'
 export async function expectPageUrl (page, locator, url) {
   const link = page.locator(locator)
 
-  await link.click()
-  await page.waitForURL(url, { waitUntil: 'commit' })
-
-  expect(page.url()).toBe(url)
+  await expect(link).toHaveAttribute('href', url)
 }
