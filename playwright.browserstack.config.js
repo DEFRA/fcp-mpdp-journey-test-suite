9f6a2cffb03fdd149945fbd7ab274c3a3393b2dd
John Watson <john.watson1@defra.gov.uk>
2026-07-13 12:07:07 +0100

    Update proxy value

--- playwright.browserstack.config.js
+++ playwright.browserstack.config.js
@@ -3,12 +3,10 @@ import { ProxyAgent, setGlobalDispatcher } from 'undici'
 import { bootstrap } from 'global-agent'
 import baseConfig from './playwright.config.js'
 
-if (process.env.CDP_HTTP_PROXY) {
-  const proxyUrl = process.env.CDP_HTTP_PROXY
-  setGlobalDispatcher(new ProxyAgent({ uri: proxyUrl }))
-  bootstrap()
-  globalThis.GLOBAL_AGENT.HTTP_PROXY = proxyUrl
-}
+const proxyUrl = 'http://localhost:3128'
+setGlobalDispatcher(new ProxyAgent({ uri: proxyUrl }))
+bootstrap()
+globalThis.GLOBAL_AGENT.HTTP_PROXY = proxyUrl
 
 export default defineConfig({
   ...baseConfig
